f68fb4efbcd6963cae062d862ab3e64a5fb18f8a
noahbino <noahiarrobino@yahoo.com>
2026-09-23 17:59:22 -0400

    Bind to 0.0.0.0, set display name, write xunit output to test-results.xml

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 8082;
 
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World. My name is Vincent Danh');
+	  res.end('Hello World. My name is Noah Iarrobino');
 });
 
 server.listen(port, hostname, () => {
